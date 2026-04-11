6a55a59d79276bdca1f65718e345b17069ecc489
Sam Anglin <sam@mizouzie.dev>
2026-04-11 15:27:20 +0300

    Use playwright image and bolster test runner config

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,7 +1,13 @@
 import { chromeLauncher } from '@web/test-runner-chrome';
 
 export default {
-  files: 'src/**/*.test.js',
-  browsers: [chromeLauncher()],
-  nodeResolve: true,
+	files: 'src/**/*.test.js',
+	browsers: [
+		chromeLauncher({
+			launchOptions: {
+				args: ['--no-sandbox', '--disable-dev-shm-usage'],
+			},
+		}),
+	],
+	nodeResolve: true,
 };
