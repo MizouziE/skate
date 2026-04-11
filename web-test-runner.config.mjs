5e12cceb9624439078f0365d494f7d51cf1f3c20
Sam Anglin <sam@mizouzie.dev>
2026-04-11 15:41:06 +0300

    Don't need the config

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -2,12 +2,6 @@ import { chromeLauncher } from '@web/test-runner-chrome';
 
 export default {
 	files: 'src/**/*.test.js',
-	browsers: [
-		chromeLauncher({
-			launchOptions: {
-				args: ['--no-sandbox', '--disable-dev-shm-usage'],
-			},
-		}),
-	],
+	browsers: [chromeLauncher()],
 	nodeResolve: true,
 };
